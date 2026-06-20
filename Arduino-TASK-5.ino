61145ed3d7981c21ab809784a02a295d3299c6f8
DaniDev <amoahagyemangdaniel691@gmail.com>
2026-06-20 14:45:22 -0700

    Update Arduino-TASK-5.ino

--- Arduino-TASK-5.ino
+++ Arduino-TASK-5.ino
@@ -3,30 +3,26 @@ int Led = 13;
 
 void flashLED(int times){
     for(int i = 1 ; i <= times; i++){
-      Serial.print("Blink number: ");
-      Serial.println(i);
     digitalWrite(Led,HIGH);
     delay(500);
     digitalWrite(Led,LOW);
     delay(500);
-    //times--;
-
     }
   }
 
 void setup() {
   Serial.begin(9600);
   pinMode(Led, OUTPUT);
-
+    int count = startValue;
   Serial.println("=== Smart Countdown Starting ===");
-    while(startValue > 0){
+    while(count > 0){
     Serial.print("Count: ");
-    Serial.println(startValue);
+    Serial.println(count);
 
-    flashLED(startValue);
+    flashLED(count);
     delay(1000);
 
-    startValue--;
+    count--;
   }
   Serial.println("=== Countdown Complete ===" );
 }
